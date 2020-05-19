998138bd79d7a927dfc95dca8f957cb2ff8ea451
joel alva <panthersjoe@hotmail.com>
2020-05-19 17:56:10 -0400

    first commit

---
+++ src/Components/AddItemForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default class AddItemForm extends React.Component {
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    this.props.onAddItem(e.target.itemToAdd.value);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitForm}>
+        <input
+          name="itemToAdd"
+          type="text"
+          placeholder="carrots"
+          aria-label="list item"
+        />
+        <button type="submit" style={{ padding: "10px" }}>
+          add
+        </button>
+      </form>
+    );
+  }
+}
